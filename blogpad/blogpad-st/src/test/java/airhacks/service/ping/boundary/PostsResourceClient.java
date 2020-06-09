d673af1d581c33771c0248d4a68e1d898522b772
Adam Bien <abien@adam-bien.com>
2020-06-09 19:42:48 +0200

    120_put_system_test

--- blogpad/blogpad-st/src/test/java/airhacks/service/ping/boundary/PostsResourceClient.java
+++ blogpad/blogpad-st/src/test/java/airhacks/service/ping/boundary/PostsResourceClient.java
@@ -1,16 +1,20 @@
 package airhacks.service.ping.boundary;
 
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("ping")
-public interface PingResourceClient {
+@Path("posts")
+public interface PostsResourceClient {
 
-    @GET
-    Response ping();
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    Response save(JsonObject post);
     
 }
--- blogpad/blogpad-st/src/test/java/airhacks/service/ping/boundary/PostsResourceIT.java
+++ blogpad/blogpad-st/src/test/java/airhacks/service/ping/boundary/PostsResourceIT.java
@@ -1,5 +1,8 @@
 package airhacks.service.ping.boundary;
 import java.net.URI;
+
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.ws.rs.core.Response;
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,9 +15,9 @@ import org.junit.jupiter.api.Test;
  *
  * @author airhacks.com
  */
-public class PingResourceIT {
+public class PostsResourceIT {
 
-    private PingResourceClient client;
+    private PostsResourceClient client;
 
     @BeforeEach
     public void init() {
@@ -22,18 +25,19 @@ public class PingResourceIT {
         this.client = RestClientBuilder.
                 newBuilder().
                 baseUri(uri).
-                build(PingResourceClient.class);
+                build(PostsResourceClient.class);
 
     }
 
     @Test
-    public void ping() {
-        Response response = this.client.ping();
+    public void save() {
+        JsonObject post = Json.createObjectBuilder().
+                add("title", "remote_hello").
+                add("content", "first st").
+        build();
+        Response response = this.client.save(post);
         int status = response.getStatus();
-        assertEquals(200, status);
-        String message = response.readEntity(String.class);
-        assertNotNull(message);
-        System.out.println(message);
+        assertEquals(204, status);
         
     }
 }
