ffa240d714d3978e183d798e614cd1517a0f89c6
Adam Bien <abien@adam-bien.com>
2020-06-21 10:44:48 +0200

    320_readonly_and_required_schema

--- blogpad/blogpad/src/main/java/airhacks/blogpad/posts/entity/Post.java
+++ blogpad/blogpad/src/main/java/airhacks/blogpad/posts/entity/Post.java
@@ -4,17 +4,24 @@ import java.time.LocalDateTime;
 
 import javax.validation.constraints.Size;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 public class Post {
 
+    @Schema(readOnly = true)
     public String fileName;
     
+    @Schema(required = true)
     @Size(min=3,max=255)
     public String title;
     
-    @Size(min=3)
+    @Size(min = 3)
+    @Schema(required = true)
     public String content;
 
+    @Schema(readOnly = true)
     public LocalDateTime createdAt;
+    @Schema(readOnly = true)
     public LocalDateTime modifiedAt;
 
     public Post(String title, String content) {
