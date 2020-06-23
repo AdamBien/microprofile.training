27e8a140c3cf8f0953e8c6bacc1805d7c75841dd
Adam Bien <abien@adam-bien.com>
2020-06-23 11:34:08 +0200

    420_creating_torture_scenarios

--- blogpad/blogpad-st/src/test/java/airhacks/blogpad/posts/boundary/PostsResourceTortureIT.java
+++ blogpad/blogpad-st/src/test/java/airhacks/blogpad/posts/boundary/PostsResourceTortureIT.java
@@ -2,6 +2,7 @@ package airhacks.blogpad.posts.boundary;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.net.URI;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.stream.Stream;
 
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
@@ -40,10 +42,25 @@ public class PostsResourceTortureIT {
 
     @Test
     public void startTorture() {
-        List<CompletableFuture<Void>> tasks = Stream.generate(() -> CompletableFuture.runAsync(this::findPost,this.threadPool)).
-                limit(500).
-                collect(Collectors.toList());
+        List<CompletableFuture<Void>> tasks = Stream
+                .generate(this::runScenario).limit(500)
+                .collect(Collectors.toList());
         tasks.forEach(CompletableFuture::join);
+    }
+
+    CompletableFuture<Void> runScenario() {
+        return CompletableFuture.runAsync(this::findPost, this.threadPool).
+        thenRunAsync(this::findNonExistingPost,
+                this.threadPool);
+    }
+    
+    void findNonExistingPost() {
+        try {            
+            this.client.findPost("not-existing" + System.nanoTime());
+            fail("should not exist");
+        } catch (WebApplicationException e) {
+        }
+    
     }
 
      void findPost() {
