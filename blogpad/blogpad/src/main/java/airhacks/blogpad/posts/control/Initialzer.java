51609c3d5db1c1fd6b801b3304cfdd8883fea754
Adam Bien <abien@adam-bien.com>
2020-06-24 11:26:11 +0200

    470_synthetic_transaction_as_health_check

--- blogpad/blogpad/src/main/java/airhacks/blogpad/posts/control/Initialzer.java
+++ blogpad/blogpad/src/main/java/airhacks/blogpad/posts/control/Initialzer.java
@@ -3,7 +3,12 @@ package airhacks.blogpad.posts.control;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
 
 import airhacks.blogpad.posts.entity.Post;
 
@@ -12,17 +17,41 @@ import airhacks.blogpad.posts.entity.Post;
 public class Initialzer {
 
 
+    static final String TITLE = "initial";
+    
     @Inject
     PostStore store;
 
     @PostConstruct
     public void installFirstPost() {
-        Post post = this.createInitialPost();
+        if(this.postExists())
+            return;
+        var post = this.createInitialPost();
         this.store.createNew(post);
     }
     
     Post createInitialPost() {
-        return new Post("initial", "welcome to blogpad");
+        return new Post(TITLE, "welcome to blogpad");
+    }
+
+    Post fetchPost() {
+        return this.store.read(TITLE);
+    }
+
+    boolean postExists() {
+        var post = this.fetchPost();
+        if (post == null)
+            return false;
+        return TITLE.equalsIgnoreCase(post.title);
+    }
+
+    @Produces
+    @Liveness
+    public HealthCheck initialExists() {
+        return () ->
+        HealthCheckResponse.named("inital-post-exists").
+                state(this.postExists()).
+         build();
     }
     
 }
