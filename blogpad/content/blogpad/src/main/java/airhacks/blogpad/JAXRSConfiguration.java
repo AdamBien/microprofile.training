9e162badbd92fd5ec29f07c6d656783f4acec63e
Adam Bien <abien@adam-bien.com>
2020-06-25 11:22:52 +0200

    .

--- blogpad/content/blogpad/src/main/java/airhacks/blogpad/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package airhacks.blogpad;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application{
-    
-}
