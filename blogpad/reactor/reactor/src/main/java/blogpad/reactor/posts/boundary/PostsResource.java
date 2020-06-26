199c865cbb97882aa958d3bc6ca6bb36f9a61a50
Adam Bien <abien@adam-bien.com>
2020-06-26 11:57:36 +0200

    550_mp_rest_client_and_mp_config

--- blogpad/reactor/reactor/src/main/java/blogpad/reactor/posts/boundary/PostsResource.java
+++ blogpad/reactor/reactor/src/main/java/blogpad/reactor/posts/boundary/PostsResource.java
@@ -1,19 +1,31 @@
 package blogpad.reactor.posts.boundary;
 
+import javax.inject.Inject;
+import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import blogpad.reactor.posts.control.PostsResourceClient;
 
 @Path("posts")
 public class PostsResource {
+
+    @Inject
+    @RestClient
+    PostsResourceClient client;
 
     @GET
     @Path("{title}")
     @Produces(MediaType.TEXT_HTML)
     public String findPost(@PathParam("title") String title){
-        return "<h1>hello</h1> " + title;
+        Response response = this.client.findPost("initial");
+        return "<h1>hello</h1> " + title + " "+ response.readEntity(JsonObject.class);
     }
 
 
---
+++ blogpad/reactor/reactor/src/main/java/blogpad/reactor/posts/control/PostsResourceClient.java
@@ -0,0 +1,26 @@
+package blogpad.reactor.posts.control;
+
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@RegisterRestClient(configKey = "content")
+@Path("posts")
+public interface PostsResourceClient {
+
+
+    @GET
+    @Path("{title}")
+    @Produces(MediaType.APPLICATION_JSON)
+    Response findPost(@PathParam("title") String title);
+    
+}
