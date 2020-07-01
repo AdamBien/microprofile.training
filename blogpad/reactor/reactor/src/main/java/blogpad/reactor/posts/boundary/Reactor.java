4392a7c5147ab98e49e565a52827dbb4fc7b706c
Adam Bien <abien@adam-bien.com>
2020-07-01 20:44:09 +0200

    770_mp_ft_fallback

--- blogpad/reactor/reactor/src/main/java/blogpad/reactor/posts/boundary/Reactor.java
+++ blogpad/reactor/reactor/src/main/java/blogpad/reactor/posts/boundary/Reactor.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 import javax.json.JsonObject;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.metrics.MetricRegistry;
 import org.eclipse.microprofile.metrics.MetricRegistry.Type;
 import org.eclipse.microprofile.metrics.annotation.RegistryType;
@@ -25,6 +26,7 @@ public class Reactor {
     @Inject
     Renderer renderer;
 
+    @Fallback(fallbackMethod = "getFallbackContent")
     public String render(String title) {
         Response response = this.client.findPost(title);
         var status = response.getStatus();
@@ -41,6 +43,18 @@ public class Reactor {
         """, postAsString);
     }
     
+
+    public String getFallbackContent(String title) {
+     return
+     """
+        <html>
+            <body>
+                <h1>Error :-( </h1>
+                <article>Error rendering post with title: %s </article>
+            </body>
+        </html>
+    """.formatted(title);
+    }
 
     
 }
