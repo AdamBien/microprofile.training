eaa823ecfae9cf993beaf8285c4ac535332f117c
Adam Bien <abien@adam-bien.com>
2020-07-02 14:30:20 +0200

    780_counting_rendering_errors

--- blogpad/reactor/reactor/src/main/java/blogpad/reactor/posts/control/Renderer.java
+++ blogpad/reactor/reactor/src/main/java/blogpad/reactor/posts/control/Renderer.java
@@ -5,13 +5,22 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
+import org.eclipse.microprofile.metrics.MetricRegistry;
+import org.eclipse.microprofile.metrics.MetricRegistry.Type;
+import org.eclipse.microprofile.metrics.annotation.RegistryType;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.PolyglotException;
 import org.graalvm.polyglot.Source;
 
 public class Renderer {
 
     Source handlebars;
+
+    @Inject
+    @RegistryType(type = Type.APPLICATION)
+    MetricRegistry registry;
 
     @PostConstruct
     public void init() {
@@ -22,8 +31,16 @@ public class Renderer {
         }
     }
  
-    
     public String render(String templateContent, String postContent) {
+        try {
+            return this.unsafeRender(templateContent, postContent);
+        } catch (PolyglotException e) {
+            this.registry.counter("rendering_errors").inc();
+            throw new IllegalStateException("Rendering error, reason: " + e.getMessage(), ex);
+        }
+    }
+    
+    String unsafeRender(String templateContent, String postContent) {
         try (Context context = Context.create("js")) {
             var bindings = context.getBindings("js");
             context.eval(this.handlebars);
